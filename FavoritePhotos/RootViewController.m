8bfd623bc0e3cf29211163bbd476d7d24424b64a
Brent Dady <brentdady@gmail.com>
2015-05-30 17:53:45 -0700

    first commit

---
+++ FavoritePhotos/RootViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  FavoritePhotos
+//
+//  Created by Brent Dady on 5/30/15.
+//  Copyright (c) 2015 Brent Dady. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+// https://api.instagram.com/v1/tags/lulz/media/recent?client_id=5c15d427b87c46e1bbc09f39ed8741e4
+// https://api.instagram.com/v1/tags/lulz/media/recent?count=10&client_id=5c15d427b87c46e1bbc09f39ed8741e4
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+@end
